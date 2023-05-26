b65263266a39f07c10ff53dc0efa4ea51d11d554
Utkarsh <utkarshpandey912@gmail.com>
2023-05-26 15:35:51 +0530

    Check from Android

--- js/scripts.js
+++ js/scripts.js
@@ -93,7 +93,7 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "35px 0"
+                    "padding": "20px 0"
                 });
                 $('header .member-actions').css({
                     "top": "26px",
